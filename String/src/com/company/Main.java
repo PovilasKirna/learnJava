6b99520cf012e3279daa2f4622eed9c338b833f1
PovilasKirna <povilas.kirna@gmail.com>
2019-10-15 08:42:00 +0300

    10.15

--- String/src/com/company/Main.java
+++ String/src/com/company/Main.java
@@ -1,14 +1,47 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Main("Rokas Bokas"));
+        check(Main());
     }
-    public static String Main(String name){
-        String first = name.substring(0,1);
-        int space = name.lastIndexOf(" ");
-        String second = name.substring(space+1,space+2);
-        return first.concat(second);
+
+    private static void check(String print) {
+        String day =print.substring(0,2);
+        String month = print.substring(3,5);
+        if(print.length() == 10) {
+            if (Integer.parseInt(day) <= 31 && Integer.parseInt(day) > 0) {
+                if(print.charAt(2) == '.'){
+                    if(Integer.parseInt(month) <= 12 && Integer.parseInt(month) > 0){
+                        if(print.charAt(5) == '.') {
+                            System.out.println("Congrats you entered the right date format");
+                        }
+                        else{
+                            System.out.println("Wrong date format");
+                        }
+                    }
+                    else{
+                        System.out.println("Wrong date format");
+                    }
+                }
+                else{
+                    System.out.println("Wrong date format");
+                }
+            }
+            else{
+                System.out.println("Wrong date format");
+            }
+        }
+        else{
+            System.out.println("Wrong date format");
+        }
+    }
+
+
+    public static String Main() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the date: ");
+        return input.nextLine();
     }
 }
