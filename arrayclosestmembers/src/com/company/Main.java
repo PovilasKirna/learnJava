0b32b4b24b1b8f47c3d3792b3ad7738cd2ed3c51
PovilasKirna <povilas.kirna@gmail.com>
2019-11-18 21:55:57 +0200

    Find closest members in an array and tell the difference

---
+++ arrayclosestmembers/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.lang.Math;
+
+
+public class Main {
+    static int[] A = {1, 4, 9, 11, 15,15};
+    public static void main(String[] args) {
+        int min = 2147483647;
+        for (int i = 0; i < A.length - 1; i++) {
+            if (Math.abs(A[i] - A[i + 1]) < min) {
+                min = Math.abs(A[i] - A[i + 1]);
+            }
+        }
+        output("Maziausias tarpas = " + min);
+    }
+    public static void output(String message) {
+        javax.swing.JOptionPane.showMessageDialog(null,message);
+    }
+}
