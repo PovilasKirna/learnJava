f4e5f665b017f8e1e0bada724909c98ffb6c82be
PovilasKirna <povilas.kirna@gmail.com>
2019-12-02 21:11:44 +0200

    cd list arrays

--- cd/src/com/company/Main.java
+++ cd/src/com/company/Main.java
@@ -1,8 +1,49 @@
 package com.company;
 
-public class Main {
+public class Main
+{
+    String[] titles = {"Cafe del Mar 18",
+            "Valtari",
+            "Nature One",
+            "Swagg",
+            "Netsky 2",
+            "DJ Kicks",
+            "Trouble"
+    };
 
-    public static void main(String[] args) {
-	// write your code here
+    public Main()
+    {
+        String name = "";
+        do
+        {
+            name = input("Type the name of the CD");
+            search(name);
+        } while(name.length() > 0);   // type nothing to quit
     }
+
+    void search(String name)
+    {
+        for(int c = 0; c < titles.length; c=c+1)
+        {
+            String names = titles[c].toLowerCase();
+            String qname = name.toLowerCase();
+
+
+            if( names.equals(qname) )
+            {
+                output("Found " + name);
+                return;                   // quits if name is found
+            }
+        }
+        output("Not found - you don't own that");
+    }
+
+    public static void main(String[] args)
+    {  new Main(); }
+
+    public String input(String prompt)
+    { return javax.swing.JOptionPane.showInputDialog(null,prompt); }
+
+    public void output(String message)
+    {  javax.swing.JOptionPane.showMessageDialog(null,message);  }
 }
